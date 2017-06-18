29dfa38e4e0a3770dc1e6f29c174260d5c98b81b
Eve Yvonne <eveyvonne@Eves-MacBook-Pro.local>
2017-06-18 09:37:17 -0400

    finally setup for the stem project

---
+++ components/FOOTER.js.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+class FOOTER extends React.Component {
+  render() {
+    return (
+      <div>This is my footer. let's try it.</div>
+    )
+  }
+}
+
+module.exports = FOOTER;
